313323f8b5bd9bb47537af6a8a10c777fb628b88
mlvillarroya <mlvillarroya@gmail.com>
2023-08-18 09:09:36 +0200

    more linting

--- demo_bbdd_creator/demo_bbdd_creator.py
+++ demo_bbdd_creator/demo_bbdd_creator.py
@@ -18,19 +18,23 @@ class DemoDatabaseTable:
         """Function to insert data in a table"""
         # SECURITY CHECKINGS
         if len(self.data) != 0:
-            raise Exception("Table already contains data. Erase it before inserting")
+            raise EnvironmentError("Table already contains data. Erase it before inserting")
         sizes = set()
         for data in data_array:
             sizes.add(len(data))
-        if len(sizes)>1: raise Exception('All data arrays must have equal length')
-        if len(data_array) != len(self.fields): raise Exception('Data arrays must have ' + str(len(self.fields)) + ' elements')
+        if len(sizes)>1:
+            raise ValueError('All data arrays must have equal length')
+        if len(data_array) != len(self.fields):
+            raise ValueError('Data arrays must have ' + str(len(self.fields)) + ' elements')
         self.data = data_array
 
     def export_into_query(self):
+        """Function to create a query to insert data into table"""
         query = 'INSERT INTO ' + self.schema_name + '.' + self.table_name + ' ('
         for i,title in enumerate(self.fields):
             query += title
-            if i < len(self.fields) - 1: query +=  ', '
+            if i < len(self.fields) - 1:
+                query +=  ', '
         query += ') VALUES\n'
         data_array = np.array(self.data)
         trasposed_array = data_array.transpose()
@@ -39,18 +43,33 @@ class DemoDatabaseTable:
             query += '('
             for j,data in enumerate(data_row):
                 query += str(data)
-                if j < len(data_row) - 1: query +=  ', '
+                if j < len(data_row) - 1:
+                    query +=  ', '
             query += ')' + final_character
         return query
-    
+
     def export_into_file(self):
-        f = aux_functs.create_file(self.table_name + '.sql')
-        f.write(self.export_into_query())
-        f.close()
-            
+        """"Function to create a file with the query to insert data into table"""
+        new_file = aux_functs.create_file(self.table_name + '.sql')
+        new_file.write(self.export_into_query())
+        new_file.close()
+
 class DemoDatabase:
-    def __init__(self,bbdd_name) -> None:
+    """Class for databases"""
+    def __init__(self,name) -> None:
         self.tables = []
-        
+        self.name = name
+
     def table_add(self,table:DemoDatabaseTable):
+        """Function to add a table into the database"""
         self.tables.append(table)
+
+    @property
+    def Name(self):
+        """Name property"""
+        return self.name
+
+    @Name.setter
+    def Name(self, value):
+        """Name setter"""
+        self.__name = value
