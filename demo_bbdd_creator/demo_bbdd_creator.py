422f781cc7f80e71a323b549caf6d242c407dba7
mlvillarroya <mlvillarroya@gmail.com>
2023-08-18 09:24:13 +0200

    added properties

--- demo_bbdd_creator/demo_bbdd_creator.py
+++ demo_bbdd_creator/demo_bbdd_creator.py
@@ -5,38 +5,58 @@ from . import aux_functs
 class DemoDatabaseTable:
     """Class for database table"""
     def __init__(self,table_name,fields:list[str],schema_name = 'public') -> None:
-        self.table_name = table_name
-        self.fields = fields
-        self.data = []
-        self.schema_name = schema_name
+        self.__table_name = table_name
+        self.__fields = fields
+        self.__data = []
+        self.__schema_name = schema_name
+
+    @property
+    def table_name(self):
+        """Property: table name"""
+        return self.__table_name
+
+    @property
+    def fields(self):
+        """Property: fields"""
+        return self.__fields
+
+    @property
+    def data(self):
+        """Property: data"""
+        return self.__data
+
+    @property
+    def schema_name(self):
+        """Property: schema name"""
+        return self.__schema_name
 
     def erase_data(self):
         """Function to erase all elements in a table"""
-        self.data = []
+        self.__data = []
 
     def insert_data(self,data_array):
         """Function to insert data in a table"""
         # SECURITY CHECKINGS
-        if len(self.data) != 0:
+        if len(self.__data) != 0:
             raise EnvironmentError("Table already contains data. Erase it before inserting")
         sizes = set()
         for data in data_array:
             sizes.add(len(data))
         if len(sizes)>1:
             raise ValueError('All data arrays must have equal length')
-        if len(data_array) != len(self.fields):
-            raise ValueError('Data arrays must have ' + str(len(self.fields)) + ' elements')
-        self.data = data_array
+        if len(data_array) != len(self.__fields):
+            raise ValueError('Data arrays must have ' + str(len(self.__fields)) + ' elements')
+        self.__data = data_array
 
     def export_into_query(self):
         """Function to create a query to insert data into table"""
-        query = 'INSERT INTO ' + self.schema_name + '.' + self.table_name + ' ('
-        for i,title in enumerate(self.fields):
+        query = 'INSERT INTO ' + self.__schema_name + '.' + self.__table_name + ' ('
+        for i,title in enumerate(self.__fields):
             query += title
-            if i < len(self.fields) - 1:
+            if i < len(self.__fields) - 1:
                 query +=  ', '
         query += ') VALUES\n'
-        data_array = np.array(self.data)
+        data_array = np.array(self.__data)
         trasposed_array = data_array.transpose()
         for i,data_row in enumerate(trasposed_array):
             final_character = ',\n' if i < (len(trasposed_array) -1) else ';'
@@ -50,7 +70,7 @@ class DemoDatabaseTable:
 
     def export_into_file(self):
         """"Function to create a file with the query to insert data into table"""
-        new_file = aux_functs.create_file(self.table_name + '.sql')
+        new_file = aux_functs.create_file(self.__table_name + '.sql')
         new_file.write(self.export_into_query())
         new_file.close()
 
@@ -65,11 +85,11 @@ class DemoDatabase:
         self.tables.append(table)
 
     @property
-    def Name(self):
+    def name(self):
         """Name property"""
         return self.name
 
-    @Name.setter
-    def Name(self, value):
+    @name.setter
+    def name(self, value):
         """Name setter"""
         self.__name = value
